3845cb181b13c716745f8cedadf285ac568c07cb
Layanne <layrodrgs@gmail.com>
2019-12-19 14:24:14 -0300

    implementando o PrimeFaces

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -9,11 +9,12 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.io.Serializable;
 import java.util.List;
 
 @ManagedBean
 @ViewScoped
-public class LoginBean {
+public class LoginBean implements Serializable {
 
 
     private Usuario usuario = new Usuario();
@@ -22,28 +23,28 @@ public class LoginBean {
         return usuario;
     }
 
-    public String efetuaLogin() {
-        System.out.println("Fazendo login do usuário "
-                + this.usuario.getEmail());
-        FacesContext context = FacesContext.getCurrentInstance();
 
+
+    public String deslogar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().remove("usuarioLogado");
+        return "login?faces-redirect=true";
+    }
+
+    public String login() {
+        System.out.println("fazendo login do usuario " + this.usuario.getEmail());
+
+        FacesContext context = FacesContext.getCurrentInstance();
         boolean existe = new UsuarioDao().existe(this.usuario);
         if (existe) {
-            context.getExternalContext().getSessionMap()
-                    .put("usuarioLogado", this.usuario);
-
+            context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
             return "index?faces-redirect=true";
         }
 
         context.getExternalContext().getFlash().setKeepMessages(true);
-        context.addMessage(null, new FacesMessage("Usuario nao encontrado"));
-        return "login?faces-redirect=true";
-    }
-    public String deslogar(){
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.getExternalContext().getSessionMap().remove("usuarioLogado");
-        return "login?faces-redirect=true";
+        context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 
+        return "login?faces-redirect=true";
     }
 
 }
